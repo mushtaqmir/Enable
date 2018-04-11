0a6703bfb0f4e3161d0014aebf78b9a5f08e02ec
anshul <anshul.kumar84@yahoo.co.in>
2018-04-11 10:01:01 +0530

    Check out Already modified

--- app/src/main/java/com/example/mushtaqmir/app4/ActivityMain.java
+++ app/src/main/java/com/example/mushtaqmir/app4/ActivityMain.java
@@ -1,26 +1,18 @@
 package com.example.mushtaqmir.app4;
 
 import android.content.Intent;
-import android.app.Activity;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.Toolbar;
-import android.view.WindowManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.io.Serializable;
-import java.util.List;
 import java.util.Locale;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends ToolBarActivity {
 
     private Button templateBtn;
     private Button chatBtn;
@@ -96,7 +88,7 @@ public class ActivityMain extends AppCompatActivity {
 
     }
    public void openTemplateActivity(){
-        Intent intent=new Intent(this,MainActivity.class);
+        Intent intent=new Intent(this,TemplateActivity.class);
         startActivity(intent);
     }
     public void openChatActivity(){
@@ -104,37 +96,7 @@ public class ActivityMain extends AppCompatActivity {
         Intent intent=new Intent(this,ChatBox.class);
         startActivity(intent);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.history: {
-                List<OrderDetails> allOrders = mydb.getAllOrders();
-                if (allOrders.isEmpty()) {
-                    showMessage("!!Alert!!", "Nothing stored");
-                } else {
-                    Intent launchOrderHistory = new Intent(ActivityMain.this, OrderHistory.class);
-                    Bundle args = new Bundle();
-                    args.putSerializable("ARRAYLIST", (Serializable) allOrders);
-                    launchOrderHistory.putExtra("BUNDLE", args);
-                    startActivity(launchOrderHistory);
-                }
-
-            }
-
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     public void showMessage(String title,String message){
         AlertDialog.Builder builder = new  AlertDialog.Builder(this);
